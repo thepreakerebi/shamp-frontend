cdb66a7740f2b8ac337f372b8fd10a935d096e06
Elvis Kerebi <kerebipreye@gmail.com>
2025-06-06 10:06:30 +0200

    added sidebar component logic

---
+++ app/(main)/(web-app)/_components/sidebar.tsx
@@ -0,0 +1,61 @@
+import { Home, ListChecks, PlayCircle, Settings } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "Home",
+    url: "/home",
+    icon: Home,
+  },
+  {
+    title: "Tests",
+    url: "/tests",
+    icon: ListChecks,
+  },
+  {
+    title: "Test Runs",
+    url: "/test-runs",
+    icon: PlayCircle,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Navigation</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url} prefetch={false}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+} 
--- app/(main)/(web-app)/layout.tsx
+++ app/(main)/(web-app)/layout.tsx
@@ -1,6 +1,18 @@
 "use client";
 import { ProtectedRoute } from '@/components/ProtectedRoute';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from './_components/sidebar';
 
 export default function WebAppLayout({ children }: { children: React.ReactNode }) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
+  return (
+    <SidebarProvider>
+      <ProtectedRoute>
+        <AppSidebar />
+        <main className="min-h-screen">
+          <SidebarTrigger />
+          {children}
+        </main>
+      </ProtectedRoute>
+    </SidebarProvider>
+  );
 } 
