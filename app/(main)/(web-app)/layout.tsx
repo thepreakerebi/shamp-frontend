85f07e0e2e04908105c69c79bca11b1d2506db99
Elvis Kerebi <kerebipreye@gmail.com>
2025-06-09 12:24:42 +0200

    added topbar

--- app/(main)/(web-app)/layout.tsx
+++ app/(main)/(web-app)/layout.tsx
@@ -4,7 +4,7 @@ import { ProtectedRoute } from '@/components/ProtectedRoute';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from './_components/sidebar';
 import { Toaster } from '@/components/ui/sonner';
-import { Navbar } from './_components/navbar';
+import { Topbar } from './_components/topbar';
 import { CreateProjectModalProvider } from './_components/create-project-modal';
 
 export default function WebAppLayout({ children }: { children: React.ReactNode }) {
@@ -15,7 +15,7 @@ export default function WebAppLayout({ children }: { children: React.ReactNode }
           <Toaster position="top-center" />
           <AppSidebar />
           <main className="min-h-screen w-full">
-            <Navbar />
+            <Topbar />
             {children}
           </main>
         </ProtectedRoute>
