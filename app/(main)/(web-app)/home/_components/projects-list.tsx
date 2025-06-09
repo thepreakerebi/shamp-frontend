d10dbed1e53494941492a983a2ee9b8adffe3039
Elvis Kerebi <kerebipreye@gmail.com>
2025-06-09 17:29:19 +0200

    updated projects list

--- app/(main)/(web-app)/home/_components/projects-list.tsx
+++ app/(main)/(web-app)/home/_components/projects-list.tsx
@@ -125,7 +125,7 @@ export function ProjectsList() {
   return (
     <>
       <section
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full"
+        className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 gap-4 w-full"
         aria-label="Projects list"
       >
         {projects.map((project: Project) => (
