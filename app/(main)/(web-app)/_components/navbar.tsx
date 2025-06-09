cd60d02156ed08279921ab9cff93ddbf82c7c202
Elvis Kerebi <kerebipreye@gmail.com>
2025-06-09 12:20:47 +0200

    added topbar

---
+++ app/(main)/(web-app)/_components/navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { Breadcrumbs } from './breadcrumbs';
+import { CreateProjectButton } from './create-project-button';
+import { usePathname } from 'next/navigation';
+
+export function Navbar() {
+  const pathname = usePathname();
+  return (
+    <section className="flex flex-row items-center justify-between p-4 w-full">
+      <section className="flex flex-row items-center gap-4">
+        <SidebarTrigger />
+        <Breadcrumbs />
+      </section>
+      <section className="flex flex-row items-center gap-4">
+        {pathname === '/home' && <CreateProjectButton />}
+        {/* Add more buttons for other pages here as needed */}
+      </section>
+    </section>
+  );
+} 
--- app/(main)/(web-app)/layout.tsx
+++ app/(main)/(web-app)/layout.tsx
@@ -1,16 +1,13 @@
 "use client";
 import React from 'react';
 import { ProtectedRoute } from '@/components/ProtectedRoute';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from './_components/sidebar';
-import { Breadcrumbs } from './_components/breadcrumbs';
-import { CreateProjectModalProvider } from './_components/create-project-modal';
 import { Toaster } from '@/components/ui/sonner';
-import { CreateProjectButton } from './_components/create-project-button';
-import { usePathname } from 'next/navigation';
+import { Navbar } from './_components/navbar';
+import { CreateProjectModalProvider } from './_components/create-project-modal';
 
 export default function WebAppLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
   return (
     <CreateProjectModalProvider>
       <SidebarProvider>
@@ -18,16 +15,7 @@ export default function WebAppLayout({ children }: { children: React.ReactNode }
           <Toaster position="top-center" />
           <AppSidebar />
           <main className="min-h-screen w-full">
-            <section className="flex flex-row items-center justify-between p-4 w-full">
-              <section className="flex flex-row items-center gap-4">
-                <SidebarTrigger />
-                <Breadcrumbs />
-              </section>
-              <section className="flex flex-row items-center gap-4">
-                {pathname === '/home' && <CreateProjectButton />}
-                {/* Add more buttons for other pages here as needed */}
-              </section>
-            </section>
+            <Navbar />
             {children}
           </main>
         </ProtectedRoute>
