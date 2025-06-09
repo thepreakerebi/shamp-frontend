46b4da00dc5f1fb94012e785423969079fe9f103
Elvis Kerebi <kerebipreye@gmail.com>
2025-06-09 12:20:58 +0200

    added topbar

--- app/(main)/(web-app)/_components/topbar.tsx
+++ app/(main)/(web-app)/_components/topbar.tsx
@@ -5,7 +5,7 @@ import { Breadcrumbs } from './breadcrumbs';
 import { CreateProjectButton } from './create-project-button';
 import { usePathname } from 'next/navigation';
 
-export function Navbar() {
+export function Topbar() {
   const pathname = usePathname();
   return (
     <section className="flex flex-row items-center justify-between p-4 w-full">
