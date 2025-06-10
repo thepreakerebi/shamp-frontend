347072f7fae7a73f2f52ca180a44afe6fd15402e
Elvis Kerebi <kerebipreye@gmail.com>
2025-06-10 17:50:38 +0200

    removed toast logic from personas page

--- app/(main)/(web-app)/personas/page.tsx
+++ app/(main)/(web-app)/personas/page.tsx
@@ -1,21 +1,8 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { toast } from "sonner";
-import { usePersonas } from "@/hooks/use-personas";
 import { PersonasList } from "./_components/personas-list";
 
 export default function PersonasPage() {
-  const { personas } = usePersonas();
-  const prevCount = useRef(personas ? personas.length : 0);
-
-  useEffect(() => {
-    if (personas && personas.length > prevCount.current) {
-      toast.success("New persona created!");
-    }
-    prevCount.current = personas ? personas.length : 0;
-  }, [personas]);
-
   return (
       <main className="p-4 w-full flex flex-col gap-8">
         <PersonasList />
