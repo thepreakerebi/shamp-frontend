dc3a58cc4437a24b7be437c3ed1ab3ca8099ac00
Elvis Kerebi <kerebipreye@gmail.com>
2025-07-01 11:46:02 +0200

    added issues store and hook

---
+++ lib/store/issues.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+
+export interface Issue {
+  _id: string;
+  testRunId: string;
+  testName: string;
+  personaName: string;
+  uiIssues: string[];
+  accessibilityIssues: string[];
+  copyIssues: string[];
+  interactionIssues: string[];
+  contextConfusion: string[];
+  resolved: boolean;
+  createdAt: string;
+}
+
+interface IssuesState {
+  issues: Issue[] | null;
+  issuesLoading: boolean;
+  issuesError: string | null;
+  setIssues: (issues: Issue[] | null) => void;
+  setIssuesLoading: (loading: boolean) => void;
+  setIssuesError: (error: string | null) => void;
+  addIssue: (issue: Issue) => void;
+  updateIssue: (issue: Issue) => void;
+  deleteIssue: (id: string) => void;
+  reset: () => void;
+}
+
+export const useIssuesStore = create<IssuesState>((set) => ({
+  issues: null,
+  issuesLoading: true,
+  issuesError: null,
+  setIssues: (issues) => set({ issues }),
+  setIssuesLoading: (issuesLoading) => set({ issuesLoading }),
+  setIssuesError: (issuesError) => set({ issuesError }),
+  addIssue: (issue) => set((state) => ({ issues: [issue, ...(state.issues ?? [])] })),
+  updateIssue: (issue) =>
+    set((state) => ({
+      issues: state.issues ? state.issues.map((i) => (i._id === issue._id ? issue : i)) : null,
+    })),
+  deleteIssue: (id) =>
+    set((state) => ({ issues: state.issues ? state.issues.filter((i) => i._id !== id) : null })),
+  reset: () => set({ issues: null, issuesLoading: true, issuesError: null }),
+})); 
